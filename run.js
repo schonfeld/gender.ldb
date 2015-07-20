09da5b1cecddcda6f881c597e60fe803127e305c
Michael Schonfeld <michael@schonfeld.org>
2015-07-20 09:20:57 -0400

    normalize db values

--- run.js
+++ run.js
@@ -3,8 +3,15 @@ var async = require('async');
 var db = levelup('./gender.ldb');
 var data = require('./data');
 
+var allowedValues = ["MALE", "MOSTLY_MALE", "FEMALE", "MOSTLY_FEMALE", "UNISEX", "UNKNOWN"];
+
 async.eachSeries(data, function iterator(item, callback) {
-  db.put(item["name"].toLowerCase() + ":" + item["country"].toLowerCase(), item["gender"].toLowerCase(), callback);
+  var value = item["gender"].toUpperCase();
+  if(allowedValues.indexOf(value) === -1) {
+    console.warn("Value not allowed for [" + item["name"] + ":" + item["country"] + "]: " + value);
+    return callback();
+  }
+  db.put(item["name"].toLowerCase() + ":" + item["country"].toLowerCase(), value, callback);
 }, function done(err) {
   if(err) {
     console.log("Oops: ", err);
